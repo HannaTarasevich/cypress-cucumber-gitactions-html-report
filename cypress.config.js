5f34d93357b7012cb26ade99cdb1f1422cdd3ca6
Hanna Tarasevich <ann.tarasevich@gmail.com>
2023-09-02 10:15:14 +0200

    Update eslint script

--- cypress.config.js
+++ cypress.config.js
@@ -1,34 +1,34 @@
-const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+const { defineConfig } = require('cypress')
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+const preprocessor = require('@badeball/cypress-cucumber-preprocessor')
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild')
+const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
-async function setupNodeEvents(on, config) {
+async function setupNodeEvents (on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  await preprocessor.addCucumberPreprocessorPlugin(on, config)
 
   on(
-    "file:preprocessor",
+    'file:preprocessor',
     createBundler({
-      plugins: [createEsbuildPlugin.default(config)],
+      plugins: [createEsbuildPlugin.default(config)]
     })
-  );
-  allureWriter(on, config);
+  )
+  allureWriter(on, config)
 
   // Make sure to return the config object as it might have been modified by the plugin.
-  return config;
+  return config
 }
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents,
-    specPattern: "cypress/e2e/features/*.feature",
-    baseUrl: "https://www.saucedemo.com",
+    specPattern: 'cypress/e2e/features/*.feature',
+    baseUrl: 'https://www.saucedemo.com',
     includeShadowDom: true,
     chromeWebSecurity: false,
     env: {
-      allureReuseAfterSpec: true,
+      allureReuseAfterSpec: true
     }
-  },
-});
+  }
+})
