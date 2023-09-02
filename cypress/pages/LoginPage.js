6e63c80ed50978a2c3ad59f75c966d512acd42a4
Hanna Tarasevich <ann.tarasevich@gmail.com>
2023-09-02 22:39:12 +0200

    Clean-up extra tests

--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -6,18 +6,6 @@ class LoginPage {
     errorMessage: () => cy.get('h3[data-test="error"]')
   }
 
-  typeUsername (username) {
-    this.elements.usernameInput().type(username)
-  }
-
-  typePassword (password) {
-    this.elements.passwordInput().type(password)
-  }
-
-  clickLogin () {
-    this.elements.loginBtn().click()
-  }
-
   submitLogin (username, password) {
     this.elements.usernameInput().type(username)
     this.elements.passwordInput().type(password)
